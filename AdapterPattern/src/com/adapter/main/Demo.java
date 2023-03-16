e513fa161e1d9254808db04ae2a13b1f7483085c
Vaibhav Jain <22928913+vaibhavajain@users.noreply.github.com>
2023-03-16 16:55:36 +1100

    Add files via upload

---
+++ AdapterPattern/src/com/adapter/main/Demo.java
@@ -0,0 +1,31 @@
+package com.adapter.main;
+
+import com.adapter.adapter.SquarePegAdapter;
+import com.adapter.round.RoundHole;
+import com.adapter.round.RoundPeg;
+import com.adapter.square.SquarePeg;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        //hole.fits(smallSqPeg); // Won't compile.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+	}
+
+}
